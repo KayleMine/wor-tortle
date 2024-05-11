834039d8a68d52b76ed0099d6ac2b4363ef4527f
KayleMine <60858771+KayleMine@users.noreply.github.com>
2024-05-11 20:20:06 +0300

    11052024

--- update/offsets.py
+++ update/offsets.py
@@ -2,7 +2,7 @@ class Offsets:
     class Base:
         game_ptr = 0x4DA3028 + 0x40  # "mem.memreport_sys"
         # entity_list_ptr = 0x4DF4A88 + 0xE0  # "m_player"
-        entity_list_ptr = 0x4E1DEA8 + 0xE0  # "m_player"
+        entity_list_ptr = 0x4E1DE48 + 0xE0  # "m_player"
         camera_control = 0x4D9C888  #
         entity_list_count = 0x10  # (entity_list_ptr +)
         local_entity_ptr = 0x18  # (entity_list_ptr +)
