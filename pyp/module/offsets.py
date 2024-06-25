dab2d08a0c3fa08ae96f579098c620b50d92d876
Xin1337 <drlftg0d321@gmail.com>
2024-06-25 23:30:48 +0800

    Update offsets.py

--- pyp/module/offsets.py
+++ pyp/module/offsets.py
@@ -1,41 +1,41 @@
 class Offsets:
     class Base:
-        game_ptr = 0x4DB00B8 + 0x40  # "mem.memreport_sys"
-        entity_list_ptr = 0x4E2B110 + 0xE0  # "m_player"
-        camera_control = 0x4DA9908  #
+        game_ptr = 0x4F50140 + 0x38  # "mem.memreport_sys"
+        entity_list_ptr = 0x4FD2AA8 + 0xE0  # "m_player"
+        camera_control = 0x4F4E798  #
         entity_list_count = 0x10  # (entity_list_ptr +)
         local_entity_ptr = 0x18  # (entity_list_ptr +)
-        game_modes = 0x4D4A7E0
-        current_game_mode = 0x4DE8EC0
+        game_modes = 0x4EF6A80
+        current_game_mode = 0x4F92DC0
 
     class Game:
         time = 0x1B4
         map_ptr = 0x1D8
-        air_units_ptr = 0x2F8
-        air_units_count = 0x308
-        ground_units_ptr = 0x310
-        ground_units_count = 0x320
-        units_ptr = 0x328
-        units_count = 0x338
-        ballistics_ptr = 0x418
-        camera_ptr = 0x608
+        air_units_ptr = 0x2E8
+        air_units_count = 0x2F8
+        ground_units_ptr = 0x300
+        ground_units_count = 0x310
+        units_ptr = 0x318
+        units_count = 0x328
+        ballistics_ptr = 0x408
+        camera_ptr = 0x5E8
 
     class Camera:
-        position = 0x68
-        view_matrix = 0x1E8
+        position = 0x58
+        view_matrix = 0x1D0
 
     class Ballistics:
-        selected_unit_ptr = 0x630
-        weapon_position = 0x1C94
-        weapon_position_two = 0x1CA0 + 0x10
-        velocity = 0x1CA0
-        mass = 0x1CAC
-        caliber = 0x1CB0
-        length = 0x1CB4
-        max_dist = 0x1CB8
-        bomb_impact_point = 0x18B4
-        bullet_impact_point = 0x2050 + 0x10
-        ingame_ballistics = 0x20B8
+        selected_unit_ptr = 0x668
+        weapon_position = 0x1D98
+        weapon_position_two = 0x1D6C
+        velocity = 0x1D78
+        mass = 0x1D84
+        caliber = 0x1D88
+        length = 0x1D8C
+        max_dist = 0x1D90
+        bomb_impact_point = 0x1974
+        bullet_impact_point = 0x2138
+        ingame_ballistics = 0x2190
 
     class Entity:
         team = 0x210
@@ -46,27 +46,27 @@ class Offsets:
         byte_size = 0x2500
         bb_min = 0x318
         bb_max = 0x324
-        reload_time = 0x968
-        rotation = 0xB94
-        position = 0xBB8
-        air_velocity_ptr = 0xBD0
-        air_velocity_offset = 0x988
+        reload_time = 0x978
+        rotation = 0xB94 + 0x10
+        position = 0xBB8 + 0x10
+        air_velocity_ptr = 0xBD8
+        air_velocity_offset = 0xB20
         air_acceleration_offset = 0x9A0
-        invul_state = 0x1208
-        flags = 0x1298
+        invul_state = 0x1218
+        flags = 0x12A8
         is_visible_byte = 0x12A2
-        state = 0x12E8
-        type = 0x12F0
-        entity_ptr = 0x12F8
-        team = 0x1380
-        class_ptr = 0x13A8
-        unit_info = 0x1390
-        weapon_info = 0x1440
+        state = 0x12F8
+        type = 0x1300
+        entity_ptr = 0x1308
+        team = 0x1398
+        class_ptr = 0x13C0
+        unit_info = 0x13A8
+        weapon_info = 0x1458
         vehicle_name_ptr = 0x8
         vehicle_class_ptr = 0x38
         # short_vehicle_name_ptr = 0x20
         short_vehicle_name_ptr = 0x8
         ground_velocity = 0x3644
-        ground_velocity_ptr = 0x2138
+        ground_velocity_ptr = 0x2178
         ground_velocity_offset = 0x54
-        damage_model_ptrs = [0x1410, 0x58, 0xA0]
+        damage_model_ptrs = [0x1428, 0x58, 0xA0]
